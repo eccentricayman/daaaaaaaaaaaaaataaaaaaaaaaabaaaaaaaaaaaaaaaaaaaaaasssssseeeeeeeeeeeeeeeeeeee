b68c70620446b31dca6a892f8f06a74c7c3ea52a
eccentricayman <aahmed3@stuy.edu>
2016-10-19 15:43:37 -0400

    NICK I FIXED IT LMFAOOOOOOOOOOOOOOOOOOOOOOOOOOO :joy: :joy: :joy: :ok_hand: :ok_hand: :ok_hand: :fire: :fire: :fire:

--- ayman.py
+++ ayman.py
@@ -11,17 +11,20 @@ selectStatement = "SELECT name, mark from students, courses WHERE students.id = 
 data = cur.execute(selectStatement)
 
 curStudent = ""
-avg = 0
 ctr = 0
+avg = 0
 
 for student in data:
-    if (student[0] == curStudent or curStudent == ""):
+    if curStudent == "":
         curStudent = student[0]
+        avg = student[1]
+        ctr = 1
+    elif student[0] == curStudent:
         ctr += 1
         avg += student[1]
-    else:
+    elif student[0] != curStudent and curStudent != "":
         print "%s has a %d average"%(curStudent, avg / ctr)
-        ctr = 0
-        avg = 0
-        curStudent = ""
-print "ayman has a SELECT mark FROM students WHERE mark < 65 gpa"
+        curStudent = student[0]
+        ctr = 1
+        avg = student[1]
+        
